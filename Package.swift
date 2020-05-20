f1fb3af1bfa2d0f3ccf7a8c2a5172080e01c3b27
Anne Cahalan <northofnormal@gmail.com>
2020-05-20 18:25:42 -0400

    initialCommit

--- Package.swift
+++ Package.swift
@@ -5,10 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "WobblesFramework",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "WobblesFramework",
+            type: .dynamic,
             targets: ["WobblesFramework"]),
     ],
     dependencies: [
